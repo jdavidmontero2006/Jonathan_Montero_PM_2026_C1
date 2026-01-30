d550d75e8df6ba35c856f06a9fe4ff6e5cc062d3
jdavidmontero2006 <jdavidmontero2006@gmail.com>
2026-01-30 06:17:00 -0400

    Agregando tareas al archivo

---
+++ Tarea2/Ejecircios_cap2/Programa2.3/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/* Promedio curso.
+El programa, al recibir como dato el promedio de un alumno en un curso universitario, escribe aprobado si su promedio es mayor o igual a 6, o reprobado en caso contrario
+
+PRO: variable tipo real.*/
+
+void main(void)
+{
+    float PRO;
+    printf("Ingrese el promedio del alumno: ");
+    scanf("%f", &PRO);
+    if (PRO >= 6)
+        printf("\nAprobado");
+    else
+        printf("\nReprobado");
+}
